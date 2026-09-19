b6be9135ff37d0c93c2d076152d3aeeaf530e496
Sanal da Silva <tomazsanal@gmail.com>
2026-09-19 01:39:21 -0300

    feat(health): implements a health check

---
+++ src/main/java/com/tom/url_shortener/health_check/HealthCheckController.java
@@ -0,0 +1,16 @@
+package com.tom.url_shortener.health_check;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
