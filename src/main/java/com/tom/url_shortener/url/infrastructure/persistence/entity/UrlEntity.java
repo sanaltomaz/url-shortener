b7040c9936f6ef63df48329c566918aad6f4913e
Sanal da Silva <tomazsanal@gmail.com>
2026-09-20 01:24:35 -0300

    refactor(persistence): decouple domain repository from spring data jpa

--- src/main/java/com/tom/url_shortener/url/infrastructure/persistence/entity/UrlEntity.java
+++ src/main/java/com/tom/url_shortener/url/infrastructure/persistence/entity/UrlEntity.java
@@ -1,17 +1,26 @@
 package com.tom.url_shortener.url.infrastructure.persistence.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tb_urls")
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UrlEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String originalUrl;
+
+    @Column(unique = true)
     private String shortCode;
 }
